2960012cd152ab17383d566afce253047576f22a
Antonio Luiz Mangueira <developer@mangueira.net>
2021-08-24 18:12:06 -0300

    feat: remove a task

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -34,6 +34,8 @@ export function TaskList() {
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const keptTasks = tasks.filter(task => task.id !== id);
+    setTasks(keptTasks);
   }
 
   return (
